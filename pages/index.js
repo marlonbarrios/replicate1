a2d7c60f4493e8631233d811a9fd543d6f4db8f9
marlonbarrios <marlon@dance-tech.net>
2023-12-07 22:44:44 +0100

    repplicate

---
+++ pages/index.js
@@ -0,0 +1,106 @@
+import { useState } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../app/page.module.css";
+
+
+const sleep = (ms) => new Promise((r) => setTimeout(r, ms));
+
+export default function Home() {
+  const [prediction, setPrediction] = useState(null);
+  const [error, setError] = useState(null);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const response = await fetch("/api/predictions", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: e.target.prompt.value,
+      }),
+    });
+
+    let prediction = await response.json();
+
+    if (response.status !== 201) {
+      setError(prediction.detail);
+      return;
+    }
+
+    setPrediction(prediction);
+
+    while (
+      prediction.status !== "succeeded" &&
+      prediction.status !== "failed"
+    ) {
+      await sleep(1000);
+      const response = await fetch("/api/predictions/" + prediction.id);
+      prediction = await response.json();
+      if (response.status !== 200) {
+        setError(prediction.detail);
+        return;
+      }
+      console.log({prediction});
+      setPrediction(prediction);
+    }
+  };
+
+  const handleDownload = () => {
+    if (prediction && prediction.output) {
+      const imageUrl = prediction.output[prediction.output.length - 1];
+      fetch(imageUrl)
+        .then(response => response.blob())
+        .then(blob => {
+          const url = window.URL.createObjectURL(blob);
+          const a = document.createElement('a');
+          a.style.display = 'none';
+          a.href = url;
+          a.download = 'downloaded-image.jpg';
+          document.body.appendChild(a);
+          a.click();
+          window.URL.revokeObjectURL(url);
+          document.body.removeChild(a);
+        })
+        .catch(() => alert('Could not download the image'));
+    }
+  };
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Replicate + Next.js</title>
+      </Head>
+
+      <p>
+        Dream something with{" "}
+        <a href="https://replicate.com/stability-ai/stable-diffusion">SDXL</a>:
+      </p>
+
+      <form className={styles.form} onSubmit={handleSubmit}>
+        <input type="text" name="prompt" placeholder="Enter a prompt to display an image" />
+        <button type="submit">Go!</button>
+      </form>
+
+      {error && <div>{error}</div>}
+
+      {prediction && (
+        <div>
+          {prediction.output && (
+            <div className={styles.imageWrapper}>
+              <Image
+                fill
+                src={prediction.output[prediction.output.length - 1]}
+                alt="output"
+                sizes='100vw'
+              />
+              <button onClick={handleDownload}>Download Image</button>
+            </div>
+          )}
+          <p>status: {prediction.status}</p>
+        </div>
+      )}
+    </div>
+  );
+}
